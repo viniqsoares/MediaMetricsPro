3a5d205c60c01a940d701f5b31061027e5301fb3
Vinícius Soares <vinicius.001@gmail.com>
2024-10-03 21:04:07 -0300

    feat(test): adicionando camada de testes unitários

---
+++ tst/MediaMetricsPro.UnitTest/Vaidations/SellerValidatorTest.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+using MediaMetricsPro.Application.DataContract.DataContract;
+using MediaMetricsPro.Application.DataContract.Messages;
+using MediaMetricsPro.Application.DataContract.Validation;
+
+namespace MediaMetricsPro.UnitTest.Vaidations;
+
+public class SellerValidatorTest
+    {
+    [Theory(DisplayName = "Testing Seller Name")]
+    [InlineData(null, "xx", "name", SellerMessages.NameIsNull)]
+    [InlineData("", "xx", "name", SellerMessages.NameIsEmpty)]
+    [InlineData(" ", "xx", "name", SellerMessages.NameIsEmpty)]
+    public void SellerName_Should_Be_Invalid(string name, string contact, string propertyName, string messageError)
+        {
+        var seller = new SellerRequest(name, contact);
+        var validator = new SellerValidation();
+
+        var result = validator.Validate(seller);
+
+        Assert.False(result.IsValid);
+        result
+            .Errors
+            .First(x => x.PropertyName == propertyName &&
+                     name == x.FormattedMessagePlaceholderValues["PropertyValue"])
+            .ErrorMessage
+            .Should()
+            .Be(messageError);
+        }
+
+    [Theory(DisplayName = "Testing Seller Contact")]
+    [InlineData("xx", null, "contact", SellerMessages.ContactIsNull)]
+    [InlineData("xx", "", "contact", SellerMessages.ContactIsEmpty)]
+    [InlineData("xx", " ", "contact", SellerMessages.ContactIsEmpty)]
+    public void SellerContact_Should_Be_Invalid(string name, string contact, string propertyName, string messageError)
+        {
+        var seller = new SellerRequest(name, contact);
+        var validator = new SellerValidation();
+
+        var result = validator.Validate(seller);
+
+        Assert.False(result.IsValid);
+        result
+            .Errors
+            .First(x => x.PropertyName == propertyName &&
+                    contact == x.FormattedMessagePlaceholderValues["PropertyValue"])
+            .ErrorMessage
+            .Should()
+            .Be(messageError);
+        }
+    }
