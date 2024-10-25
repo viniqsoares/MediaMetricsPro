d7072b9b4800f1b39a3e8adae0e548d5e36ecd5a
Vinícius Soares <vinicius.001@gmail.com>
2024-10-25 20:41:17 -0300

    feat(application): Configurando Mapster

--- src/MediaMetricsPro.Application/Mappings/MappingConfiguration.cs
+++ src/MediaMetricsPro.Application/Mappings/MappingConfiguration.cs
@@ -5,15 +5,41 @@ using MediaMetricsPro.Domain.Register;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MediaMetricsPro.Application.Mappings;
-internal static class MappingConfiguration
+public static class MappingConfiguration
 {
     public static IServiceCollection RegisterMapster(this IServiceCollection services)
     {
+        GlobalSeetings();
+
         TypeAdapterConfig<Seller, SellerResponse>
             .NewConfig()
-            .TwoWays();
-        TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+            .Map(d => d.Address, s => s.Address);
+
+        TypeAdapterConfig<SellerResponse, Seller>
+           .NewConfig()
+           .ConstructUsing(source => new Seller(source.Id, source.Name, source.Address.Adapt<Address>()));
+
+        TypeAdapterConfig<State, StateResponse>
+            .NewConfig()
+            .Map(d => d.Country, s => s.Country.Adapt<CountryResponse>());
+
+        TypeAdapterConfig<StateResponse, State>
+            .NewConfig()
+            .ConstructUsing(source => new State(source.Id, source.Name, source.Country.Adapt<Country>()));
+
+        TypeAdapterConfig<Country, CountryResponse>
+          .NewConfig();
+
+        TypeAdapterConfig<CountryResponse, Country>
+           .NewConfig()
+           .ConstructUsing(source => new Country(source.Id, source.Name));
 
         return services;
+    }
+
+    private static void GlobalSeetings()
+    {
+        TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+        TypeAdapterConfig.GlobalSettings.Default.NameMatchingStrategy(NameMatchingStrategy.Flexible);
     }
 }
